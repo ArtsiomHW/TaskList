9d8346933b315639fea875e09855c5e585f3acfd
Artsiom <artsiom.hurynovich@gmail.com>
2024-12-19 16:05:04 +0100

    Comments deleted

--- TaskList/App/SceneDelegate.swift
+++ TaskList/App/SceneDelegate.swift
@@ -19,8 +19,6 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     }
 
     func sceneDidEnterBackground(_ scene: UIScene) {
-//        (UIApplication.shared.delegate as? AppDelegate)?.saveContext()
-//        (UIApplication.shared.delegate as? StoreManager)?.saveContext()
         StoreManager.shared.saveContext()
     }
 }
